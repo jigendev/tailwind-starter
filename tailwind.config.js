c0f6e90de898e0f8984892660b3d8000defca0f6
Muzan <74571694+muuzan@users.noreply.github.com>
2022-02-25 09:47:46 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/pages/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'primary': ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
